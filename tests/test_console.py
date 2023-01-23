1c4244374cffb60201edceee5e503b0afda56167
David Surumen <davidsurumen@gmail.com>
2023-01-23 17:34:59 +0300

    added console tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -14,7 +14,6 @@ import tests
 import inspect
 import models
 from console import HBNBCommand
-from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
@@ -28,51 +27,6 @@ City = HBNBCommand
 module_doc = HBNBCommand.__doc__
 path1 = "console.py"
 path2 = "tests/test_console.py"
-
-
-class TestBaseModelDocs(unittest.TestCase):
-    """Test to check behaviors"""
-
-    @classmethod
-    def setUpClass(self):
-        """setting up tests"""
-        self.base_funcs = inspect.getmembers(BaseModel, inspect.isfunction)
-
-    def test_pep8(self):
-        """Testing pep8"""
-        for path in [path1,
-                     path2]:
-            with self.subTest(path=path):
-                errors = pycodestyle.Checker(path).check_all()
-                self.assertEqual(errors, 0)
-
-    def test_module_docstring(self):
-        """Test module docstring"""
-        self.assertIsNot(module_doc, None,
-                         "base_model.py needs a docstring")
-        self.assertTrue(len(module_doc) > 1,
-                        "base_model.py needs a docstring")
-
-    def test_class_docstring(self):
-        """Test classes doctring"""
-        self.assertIsNot(BaseModel.__doc__, None,
-                         "BaseModel class needs a docstring")
-        self.assertTrue(len(BaseModel.__doc__) >= 1,
-                        "BaseModel class needs a docstring")
-
-    def test_func_docstrings(self):
-        """test func dostrings"""
-        for func in self.base_funcs:
-            with self.subTest(function=func):
-                self.assertIsNot(
-                    func[1].__doc__,
-                    None,
-                    "{:s} method needs a docstring".format(func[0])
-                )
-                self.assertTrue(
-                    len(func[1].__doc__) > 1,
-                    "{:s} method needs a docstring".format(func[0])
-                )
 
 
 class ConsoleTest(unittest.TestCase):

bb0be20cb39ee95cd2cbaa7e8a5ec25515a8b852
David Surumen <davidsurumen@gmail.com>
2023-01-24 18:45:44 +0300

    added relationship to Place objects

--- models/city.py
+++ models/city.py
@@ -13,7 +13,7 @@ class City(BaseModel, Base):
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id', ondelete="CASCADE"), nullable=False)
 
-        #places = relationship('Place', backref='cities', cascade='delete')
+        places = relationship('Place', backref='cities', cascade='all, delete, delete-orphan')
     else:
         state_id = ""
         name = ""

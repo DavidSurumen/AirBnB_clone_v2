4607282a59dfd1299b530ef2aa9ebd767fe6c721
David Surumen <davidsurumen@gmail.com>
2023-01-25 14:37:35 +0300

    refactor

--- models/place.py
+++ models/place.py
@@ -9,14 +9,15 @@ from models.review import Review
 from sqlalchemy import Table
 
 
-relationship_table = Table('place_amenity', Base.metadata,
-                           Column('place_id', String(60),
-                                  ForeignKey('places.id'),
-                                  primary_key=True, nullable=False),
-                           Column('amenity_id', String(60),
-                                  ForeignKey('amenities.id'),
-                                  primary_key=True, nullable=False)
-                           )
+if storage_type == 'db':
+    relationship_table = Table('place_amenity', Base.metadata,
+                               Column('place_id', String(60),
+                                      ForeignKey('places.id'),
+                                      primary_key=True, nullable=False),
+                               Column('amenity_id', String(60),
+                                      ForeignKey('amenities.id'),
+                                      primary_key=True, nullable=False)
+                               )
 
 
 class Place(BaseModel, Base):

5fa5e69d7e65cda9042872d2af55ab31ec809088
David Surumen <davidsurumen@gmail.com>
2023-01-25 13:34:14 +0300

    modified to allow default attributes for all storage types

--- models/base_model.py
+++ models/base_model.py
@@ -21,20 +21,23 @@ class BaseModel:
         updated_at = Column(DateTime, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            # from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            # storage.new(self)   ->  moved to save()
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        """Instantiates a new model"""
+        # from models import storage
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        # storage.new(self)   ->  moved to save()
+
+        if kwargs:
+            for key, val in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    self.__dict__[key] = datetime.fromisoformat(val)
+
+                elif key == '__class__':
+                    pass
+
+                else:
+                    self.__dict__[key] = val
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -46,7 +49,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        storage.new()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):

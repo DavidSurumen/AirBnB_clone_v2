05da5e875ee1350df9025d25be85650cd1767292
David Surumen <davidsurumen@gmail.com>
2023-01-25 18:04:09 +0300

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-""" Flask app which lists all State objects
-"""
-from flask import Flask, render_template
-from models.state import State
+"""Simple Flask app, with additional route"""
+from flask import Flask, abort, render_template
 from models import storage
-
+from models.state import State
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route('/states_list')
-def all_states():
-    """ display in html, list of states """
-    state_list = storage.all(State)
-    return render_template('7-states_list.html', states=state_list)
+def run_all_states():
+    """Run all states"""
+    l_list = storage.all(State)
+    return render_template('7-states_list.html', l_list=l_list)
 
 
 @app.teardown_appcontext
-def tearDown(self):
-    """ removes the current sqlalchemy session. """
+def do_teardown(self):
+    """Closes session"""
     storage.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
